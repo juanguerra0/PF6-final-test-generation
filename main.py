60990ef5a97ad3317f4b98ad1f4983a41b2cf870
juanguerra0 <juan.guerra@genstudents.org>
2025-12-09 13:03:31 -0500

    Subida solución

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import json
+import requests
+
+
+resultado = requests.get("https://api-colombia.com/api/v1/TypicalDish")
+platos = json.loads(resultado.content)
+
+
+
+
+def dish_fetch(eleccion):
+   #eleccion = int(input())
+   respuesta = platos[eleccion]
+
+   return respuesta
+
+
+
+
 def main():
-  print("Hello learners!")
+
+    #print("¡Hola! Bienvenido al programa")
+    #print("Selecciona el plato del cuál quieres saber mas información (Numero del 1 al )")
+
+    eleccion = int(input())
+    
+    print(dish_fetch(eleccion))
+
+    #print("¡Hola, estudiantes!")
+
 
 if __name__=="__main__":
-  main()
+    main()  
